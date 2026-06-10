e0bf50a4f45db758ae0a0cb2ae40aa23a476e6ba
Solanki777 <solankimaheshkhash7@gmail.com>
2026-06-10 21:30:43 +0530

    migrations

--- core/home/models.py
+++ core/home/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Studnets(models.Model):
+
+
+    # name should be length in 100 characters
+    # id = models.AutoField() #django will automatticalyy add this field to track multiple models
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    email = models.EmailField()
+    address = models.TextField(null=True, blank=True 
+    )
+    image = models.ImageField()
+    file = models.FileField()
+
+# after creating any model or doing any changes in the model schemas the you have to hit the command
+class Product(models.Model):
+    pass
